84e351ab5ddc629d514f3e2822a48857043ee1a6
Anca Puscasu <ancapuscasu@ymail.com>
2022-05-04 13:57:54 -0400

    created MessagesScreen to show messages as ListItems in FlatList component

---
+++ app/screens/MessagesScreen.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { FlatList } from "react-native";
+
+import Screen from "../components/Screen";
+import ListItem from "../components/ListItem";
+import ListItemSeparator from "../components/ListItemSeparator";
+import ListItemDeleteAction from "../components/ListItemDeleteAction";
+
+const initialMessages = [
+  {
+    id: 1,
+    title: "T1",
+    description: "D1",
+    image: require("../assets/headshot.png"),
+  },
+  {
+    id: 2,
+    title: "T2",
+    description: "D2",
+    image: require("../assets/headshot.png"),
+  },
+];
+
+function MessagesScreen() {
+  const [messages, setMessages] = useState(initialMessages);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const handleDelete = (message) => {
+    //Delete message from messages array
+    setMessages(messages.filter((m) => m.id !== message.id));
+  };
+
+  return (
+    <Screen>
+      <FlatList
+        data={messages}
+        keyExtractor={(message) => message.id.toString()}
+        renderItem={({ item }) => (
+          <ListItem
+            title={item.title}
+            subTitle={item.description}
+            image={item.image}
+            onPress={() => console.log("message selected", item)}
+            renderRightActions={() => (
+              <ListItemDeleteAction onPress={() => handleDelete(item)} />
+            )}
+          />
+        )}
+        ItemSeparatorComponent={ListItemSeparator}
+        refreshing={refreshing}
+        onRefresh={() =>
+          setMessages([
+            {
+              id: 1,
+              title: "T1",
+              description: "D1",
+              image: require("../assets/headshot.png"),
+            },
+          ])
+        }
+      />
+    </Screen>
+  );
+}
+
+export default MessagesScreen;
