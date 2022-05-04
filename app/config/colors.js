adf2a590fe17c96912dbf5ea2feb1d2ff65be45b
Anca Puscasu <ancapuscasu@ymail.com>
2022-05-04 13:53:37 -0400

    added colors to default colors

--- app/config/colors.js
+++ app/config/colors.js
@@ -1,9 +1,11 @@
 export default {
   primary: "#fc5c65",
   secondary: "#4ECDC4",
+  logout: "#ffe66d",
   black: "#000",
   white: "#fff",
   lightGrey: "#F2F2F2",
   textColor: "#434343",
   mediumGrey: "#6E6969",
+  danger: "#ff5252",
 };
